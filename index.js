80c9decb5b7e9fb89e34c07c71cfc603fa07777b
Lyka <66162537+lykaoclarit@users.noreply.github.com>
2021-09-06 06:25:28 +0800

    Update index.js

--- index.js
+++ index.js
@@ -74,3 +74,51 @@ if(year % 4 === 0){
 }
 }
 
+//arrays
+var guestList = ["Lyka", "Ori", "Chumi", "Rooney", "Chumchum"];
+
+var guest = prompt("Hello! What is your name?");
+
+if (guestList.includes(guest) === true){
+    alert("Welcome our beloved guest!");
+}else{
+    alert("Sorry, maybe next time.");
+}
+
+//counter
+var output = [];
+var count = 1;
+
+function generateCount(){
+    output.push(count);
+    count++;
+
+    console.log(output);
+}
+
+
+//Fizz Buzz
+var output = [];
+var count = 1; 
+
+
+function FizzBuzz(){
+
+if (count % 3 === 0 && count % 5 === 0){
+     output.push("FizzBuzz");
+}
+
+else if (count % 3 === 0){
+    output.push("Fizz");
+}
+else if (count % 5 === 0){
+   output.push("Buzz");
+}
+
+ else{
+   output.push(count); 
+}
+
+count++;
+console.log(output);
+}
